bb4d35012618be55f0a7e05f80a67a40a99d1143
Admin <Admin@DESKTOP-V35440L>
2021-06-18 12:36:08 -0500

    //SE CREO LA CLASE EMPLEADO CON GETTERS AND SETTERS

---
+++ ProjectTestGit/src/org/modelo/empleado/Empleado.java
@@ -0,0 +1,38 @@
+package org.modelo.empleado;
+
+import java.sql.Date;
+
+public class Empleado {
+	
+	private int id;
+	private String nombre;
+	private double salario;
+	private Date fecha;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	
+
+}
